432c0057c6e5eafab629ac65de6114924e6e6f04
smouhamed472 <smouhamed47260@gmail.com>
2025-02-10 19:07:39 +0000

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request,id):
+    post=get_object_or_404(Post,id=id)
+    print("#==="*40)
+    print(post)
+    print("#==="*40)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+# Create your views here.
